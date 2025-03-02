cb58ed87fe014444b874ae92585068466bfca27a
weizhusong2024 <weizhusong2024@163.com>
2025-03-02 10:47:21 +0800

    first commit

---
+++ yufa/struct.h
@@ -0,0 +1,31 @@
+// struct.h
+#ifndef STRUCT_H
+#define STRUCT_H
+
+#include <string>
+#include <map>
+#include <vector>
+
+// 定义Token结构
+enum TokenType {
+    IDENFR, INTCON, CHARCON, STRCON, CONSTTK, INTTK, CHARTK, VOIDTK, MAINTK, IFTK,
+    ELSETK, SWITCHTK, CASETK, DEFAULTTK, WHILETK, FORTK, SCANFTK, PRINTFTK, RETURNTK,
+    PLUS, MINU, MULT, DIV, ASSIGN, LSS, LEQ, GRE, GEQ, EQL, NEQ, COLON,
+    SEMICN, COMMA, LPARENT, RPARENT, LBRACK, RBRACK, LBRACE, RBRACE,
+    MOD, OR, AND, NOT, GETINTTK, BREAKTK, CONTINUETK
+};
+
+struct Token {
+    TokenType type;
+    std::string value;
+    int line;
+};
+
+// 单词名称和类别码映射表
+extern std::map<std::string, TokenType> keywords;
+
+// 单字符操作符映射表
+extern std::map<char, TokenType> singleCharTokens;
+
+#endif // STRUCT_H
+
